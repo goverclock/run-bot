fe3ed6ed5cf4b641dd99f8a498d3892fcf4adb8b
goverclock <goverclock@outlook.com>
2022-08-22 20:30:48 +0800

    Update index_remote.js

--- index_remote.js
+++ index_remote.js
@@ -9,7 +9,12 @@ let client = bot;
 client.on("system.login.slider", function (e) {
 	console.log("输入ticket：")
 	process.stdin.once("data", ticket => this.submitSlider(String(ticket).trim()))
-  }).login("password")
+  }).login("password")	// 注意下面一处的"password"也需要替换
+
+// 为了防止不小心登陆了bot帐号而把服务器踢下线,下面的代码使服务器被踢时自动重新登陆
+client.on("system.offline.kickoff", function (e) {
+	client.login("password");
+})
 
 exports.bot = bot
 
